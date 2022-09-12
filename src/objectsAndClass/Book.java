f66a20a371fc31d8cf2cdad4ad47ad19f63b533c
SizovaTati <sizoffa25@gmail.com>
2022-09-12 22:03:03 +0300

    homework 1.9

---
+++ src/objectsAndClass/Book.java
@@ -0,0 +1,29 @@
+package objectsAndClass;
+
+public class Book {
+    private final String name;
+    private final Author author;
+    private Integer publicationYear;
+
+    public Book(String name, Author author, Integer publicationYear){
+        this.name = name;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+    public String getName(){
+        return name;
+    }
+    public Author getAuthor(){
+        return author;
+    }
+    public Integer getPublicationYear(){
+        return publicationYear;
+    }
+    public void setPublicationYear(Integer publicationYear){
+        this.publicationYear = publicationYear;
+    }
+    public String toString(){
+        return "Книга \"" + name + "\" была опубликована в " + publicationYear +
+                " году. Автор " + author;
+    }
+}
